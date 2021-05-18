67596cf7bd5f68245fb6c5ee49f057a761e2b956
Kaiden42 <gitlab@tinysn.com>
2021-05-18 14:37:08 +0200

    Add unprotect function for masterkey

--- core/src/model/master_key.rs
+++ core/src/model/master_key.rs
@@ -4,7 +4,7 @@ use getset::Getters;
 use std::path::Path;
 use zeroize::Zeroize;
 
-use crate::{cryptography::decrypt_masterkey, error::PWDuckCoreError};
+use crate::{MemKey, SecVec, cryptography::{decrypt_masterkey, derive_key_protection, unprotect_masterkey}, error::PWDuckCoreError};
 /// In-memory encrypted master key
 #[derive(Clone, Debug, Zeroize)]
 #[zeroize(drop)]
@@ -25,6 +25,18 @@ impl MasterKey {
     ) -> Result<Self, PWDuckCoreError> {
         let dto = crate::io::load_masterkey(path)?;
         decrypt_masterkey(&dto, password, key_protection, nonce)
+    }
+
+    /// TODO
+    pub fn as_unprotected(
+        &self,
+        mem_key: &MemKey,
+        salt: &str,
+        nonce: &[u8],
+    ) -> Result<SecVec<u8>, PWDuckCoreError> {
+        let key_protection = derive_key_protection(mem_key, salt)?;
+        let masterkey = unprotect_masterkey(&self.key, &key_protection, nonce)?;
+        Ok(masterkey)
     }
 }
 
