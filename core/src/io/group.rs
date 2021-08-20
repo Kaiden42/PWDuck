2c09d0b5b0696c6690e87a00212a2c6548b98cb6
Kaiden42 <gitlab@tinysn.com>
2021-08-20 15:59:20 +0200

    Improve tests of groups io

--- core/src/io/group.rs
+++ core/src/io/group.rs
@@ -58,3 +58,84 @@ pub fn load_all_groups(path: &Path) -> Result<Vec<Group>, PWDuckCoreError> {
         .map(|content| ron::from_str(content).map_err(PWDuckCoreError::from))
         .collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use tempfile::tempdir;
+
+    use crate::{
+        dto::group::Group,
+        io::{create_new_vault_dir, GROUPS_DIR},
+        model::uuid,
+        Uuid,
+    };
+
+    use super::{load_all_groups, load_group, save_group};
+
+    #[test]
+    fn save_and_load_group() {
+        let dir = tempdir().unwrap();
+        let path = dir.path();
+        create_new_vault_dir(&path).unwrap();
+
+        let group = Group::new("IV".into(), "CONTENT".into());
+        let uuid: Uuid = [21_u8; uuid::SIZE].into();
+
+        save_group(&path, &uuid, &group).expect("Saving group should not fail.");
+
+        let loaded = load_group(&path, &uuid).expect("Loading group should not fail.");
+
+        assert_eq!(group.iv(), loaded.iv());
+        assert_eq!(group.content(), loaded.content());
+    }
+
+    #[test]
+    fn save_and_load_all_groups() {
+        let dir = tempdir().unwrap();
+        let path = dir.path();
+        create_new_vault_dir(&path).unwrap();
+
+        let mut groups: Vec<(Group, Uuid)> = std::iter::successors(Some(0_u8), |n| Some(n + 1))
+            .take(10)
+            .map(|n| {
+                let group = Group::new(format!("IV: {}", n), format!("Group: {}", n));
+                let uuid: Uuid = [n; uuid::SIZE].into();
+
+                save_group(&path, &uuid, &group).unwrap();
+
+                (group, uuid)
+            })
+            .collect();
+
+        let mut loaded = load_all_groups(&path).expect("Loading all groups should not fail.");
+
+        groups.sort_by(|a, b| a.0.iv().cmp(&b.0.iv()));
+        loaded.sort_by(|a, b| a.iv().cmp(&b.iv()));
+
+        assert_eq!(groups.len(), loaded.len());
+        groups.iter().zip(loaded.iter()).for_each(|((a, _), b)| {
+            assert_eq!(a.iv(), b.iv());
+            assert_eq!(a.content(), b.content());
+        })
+    }
+
+    #[test]
+    fn delete_group() {
+        let dir = tempdir().unwrap();
+        let path = dir.path();
+        create_new_vault_dir(&path).unwrap();
+
+        let group = Group::new("IV".into(), "CONTENT".into());
+        let uuid: Uuid = [42_u8; uuid::SIZE].into();
+
+        save_group(&path, &uuid, &group).unwrap();
+
+        let group_path = path.join(GROUPS_DIR).join(uuid.base64hash());
+
+        assert!(group_path.exists());
+
+        super::delete_group(&path, &uuid).expect("Deletion of group should not fail.");
+
+        assert!(!group_path.exists());
+    }
+}
