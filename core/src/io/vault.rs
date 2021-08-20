6da9f7575f957fdc68e1bf56a9c3d1c03998c3d5
Kaiden42 <gitlab@tinysn.com>
2021-08-20 16:39:15 +0200

    Improve tests of vault io

--- core/src/io/vault.rs
+++ core/src/io/vault.rs
@@ -15,3 +15,36 @@ pub fn create_new_vault_dir(path: &Path) -> Result<(), PWDuckCoreError> {
     fs::create_dir_all(path.join(ENTRIES_DIR).join(BODY))?;
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use tempfile::tempdir;
+
+    use crate::io::{create_new_vault_dir, BODY, ENTRIES_DIR, GROUPS_DIR, HEAD};
+
+    #[test]
+    fn vault_dir_creation() {
+        let dir = tempdir().unwrap();
+        let path = dir.path();
+
+        let expected = path.join("DuckDuckVault");
+        let expected_groups = expected.join(GROUPS_DIR);
+        let expected_entries = expected.join(ENTRIES_DIR);
+        let expected_entrie_heads = expected_entries.join(HEAD);
+        let expected_entrie_bodies = expected_entries.join(BODY);
+
+        assert!(!expected.exists());
+        assert!(!expected_groups.exists());
+        assert!(!expected_entries.exists());
+        assert!(!expected_entrie_heads.exists());
+        assert!(!expected_entrie_bodies.exists());
+
+        create_new_vault_dir(&expected).expect("Creation of new vault dir should not fail.");
+
+        assert!(expected.exists());
+        assert!(expected_groups.exists());
+        assert!(expected_entries.exists());
+        assert!(expected_entrie_heads.exists());
+        assert!(expected_entrie_bodies.exists());
+    }
+}
