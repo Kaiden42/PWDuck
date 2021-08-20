6c66c1eb3906c5bdc8eba455d423cbe737ccea6a
Kaiden42 <gitlab@tinysn.com>
2021-08-20 16:05:18 +0200

    Improve tests of master_key io

--- core/src/io/master_key.rs
+++ core/src/io/master_key.rs
@@ -25,3 +25,29 @@ pub fn load_masterkey(path: &Path) -> Result<MasterKey, PWDuckCoreError> {
     let content = fs::read_to_string(path.join(MASTERKEY_NAME))?;
     Ok(ron::from_str(&content)?)
 }
+
+#[cfg(test)]
+mod tests {
+    use tempfile::tempdir;
+
+    use crate::{dto::master_key::MasterKey, io::create_new_vault_dir};
+
+    use super::{load_masterkey, save_masterkey};
+
+    #[test]
+    fn save_and_load_masterkey() {
+        let dir = tempdir().unwrap();
+        let path = dir.path();
+        create_new_vault_dir(&path).unwrap();
+
+        let master_key = MasterKey::new("SALT".into(), "IV".into(), "ENCRYPTED_KEY".into());
+
+        save_masterkey(&path, master_key.clone()).expect("Saving master key should not fail.");
+
+        let loaded = load_masterkey(&path).expect("Loading master key should not fail.");
+
+        assert_eq!(master_key.salt(), loaded.salt());
+        assert_eq!(master_key.iv(), loaded.iv());
+        assert_eq!(master_key.encrypted_key(), loaded.encrypted_key());
+    }
+}
