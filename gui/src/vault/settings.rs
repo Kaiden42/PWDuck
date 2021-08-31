ea2ae000949f7de0dda16bf6ba9efb802b941c5d
Kaiden42 <gitlab@tinysn.com>
2021-08-31 15:49:27 +0200

    Improve tests of settings view

--- gui/src/vault/settings.rs
+++ gui/src/vault/settings.rs
@@ -2,16 +2,46 @@
 
 use iced::{Column, Command, Radio, Text};
 use iced_focus::Focus;
+use pwduck_core::ApplicationSettings;
 
+use crate::error::PWDuckGuiError;
 use crate::Component;
 use crate::{
     utils::centered_container_with_column, DEFAULT_COLUMN_PADDING, DEFAULT_COLUMN_SPACING,
     DEFAULT_HEADER_SIZE,
 };
 
+#[cfg(test)]
+use mocktopus::macros::*;
+
 /// The state of the settings view.
 #[derive(Debug, Focus)]
 pub struct Settings {}
+
+#[cfg_attr(test, mockable)]
+impl Settings {
+    /// Update the application settings's theme and replace it with the given value.
+    #[allow(clippy::unused_self)]
+    fn update_theme(
+        &mut self,
+        application_settings: &mut ApplicationSettings,
+        theme: pwduck_core::theme::Theme,
+    ) -> Command<SettingsMessage> {
+        application_settings.set_theme(theme);
+        Command::none()
+    }
+
+    /// Save the application settings to disk.
+    // TODO: maybe async
+    #[allow(clippy::unused_self)]
+    fn save_application_settings(
+        &mut self,
+        application_settings: &ApplicationSettings,
+    ) -> Result<(), PWDuckGuiError> {
+        pwduck_core::save_application_settings(application_settings)?;
+        Ok(())
+    }
+}
 
 /// The message produced by the settings view.
 #[derive(Clone, Debug)]
@@ -39,14 +69,11 @@ impl Component for Settings {
         _modal_state: &mut iced_aw::modal::State<crate::ModalState>,
         _clipboard: &mut iced::Clipboard,
     ) -> Result<iced::Command<Self::Message>, crate::error::PWDuckGuiError> {
-        match message {
-            SettingsMessage::ThemeChanged(theme) => {
-                application_settings.set_theme(theme);
-            }
-        }
-        // TODO: maybe async
-        pwduck_core::save_application_settings(application_settings)?;
-        Ok(Command::none())
+        let cmd = match message {
+            SettingsMessage::ThemeChanged(theme) => self.update_theme(application_settings, theme),
+        };
+        self.save_application_settings(application_settings)?;
+        Ok(cmd)
     }
 
     fn view<P: crate::Platform + 'static>(
@@ -73,5 +100,89 @@ impl Component for Settings {
             ));
 
         centered_container_with_column(vec![theme_column.into()], theme).into()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{cell::RefCell, collections::HashMap};
+
+    use iced::Command;
+    use mocktopus::mocking::*;
+
+    use crate::{Component, TestPlatform};
+
+    use super::{Settings, SettingsMessage};
+
+    thread_local! {
+        static CALL_MAP: RefCell<HashMap<String, usize>> = RefCell::new(HashMap::new());
+    }
+
+    const UPDATE_THEME: &str = "update_theme";
+    const SAVE_APPLICATION_SETTINGS: &str = "save_application_settings";
+
+    #[test]
+    fn update_theme() {
+        let mut settings = Settings::new(());
+        let mut application_settings = pwduck_core::ApplicationSettings::default();
+        assert_eq!(
+            application_settings.theme(),
+            &pwduck_core::theme::Theme::Light
+        );
+
+        let _cmd =
+            settings.update_theme(&mut application_settings, pwduck_core::theme::Theme::Dark);
+
+        assert_eq!(
+            application_settings.theme(),
+            &pwduck_core::theme::Theme::Dark
+        );
+    }
+
+    #[test]
+    fn update() {
+        let mut settings = Settings::new(());
+        let mut application_settings = pwduck_core::ApplicationSettings::default();
+        let mut modal_state = iced_aw::modal::State::new(crate::ModalState::default());
+        // WARNING: This is highly unsafe!
+        let mut clipboard: &mut iced::Clipboard = unsafe { &mut *(std::ptr::null_mut()) };
+
+        CALL_MAP.with(|call_map| unsafe {
+            call_map.borrow_mut().insert(UPDATE_THEME.to_owned(), 0);
+            call_map
+                .borrow_mut()
+                .insert(SAVE_APPLICATION_SETTINGS.to_owned(), 0);
+
+            Settings::update_theme.mock_raw(|_self, _settings, _theme| {
+                call_map.borrow_mut().get_mut(UPDATE_THEME).map(|c| *c += 1);
+                MockResult::Return(Command::none())
+            });
+            Settings::save_application_settings.mock_raw(|_self, _settings| {
+                call_map
+                    .borrow_mut()
+                    .get_mut(SAVE_APPLICATION_SETTINGS)
+                    .map(|c| *c += 1);
+                MockResult::Return(Ok(()))
+            });
+
+            // Update theme
+            assert_eq!(call_map.borrow()[UPDATE_THEME], 0);
+            assert_eq!(call_map.borrow()[SAVE_APPLICATION_SETTINGS], 0);
+            let _ = settings.update::<TestPlatform>(
+                SettingsMessage::ThemeChanged(pwduck_core::theme::Theme::Dark),
+                &mut application_settings,
+                &mut modal_state,
+                &mut clipboard,
+            );
+            assert_eq!(call_map.borrow()[UPDATE_THEME], 1);
+            assert_eq!(call_map.borrow()[SAVE_APPLICATION_SETTINGS], 1);
+
+            assert!(call_map
+                .borrow()
+                .iter()
+                .filter(|(k, _)| k.as_str() != SAVE_APPLICATION_SETTINGS)
+                .all(|(_, v)| *v == 1));
+            assert_eq!(call_map.borrow()[SAVE_APPLICATION_SETTINGS], 1);
+        })
     }
 }
