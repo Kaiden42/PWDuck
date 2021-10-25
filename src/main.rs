5a228c36b84fae99c599d30481d13a56bc58df61
Kaiden42 <gitlab@tinysn.com>
2021-10-25 16:37:25 +0200

    Move desktop implementation into desktop module

--- src/main.rs
+++ src/main.rs
@@ -47,106 +47,114 @@
     clippy::module_name_repetitions
 )]
 
-use std::path::PathBuf;
-
-use async_trait::async_trait;
-use enigo::KeyboardControllable;
-use pwduck_gui::{
-    error::{NfdError, PWDuckGuiError},
-    PWDuckGui, Platform, Sequence,
-};
-use rfd::AsyncFileDialog;
+use desktop::Desktop;
+use pwduck_gui::PWDuckGui;
 
 fn main() {
     PWDuckGui::<Desktop>::start().expect("Should not fail");
 }
 
-/// An empty placeholder struct to implement the [`Platform`](Platform) trait for.
-#[derive(Default)]
-struct Desktop;
+/// [`Platform`](pwduck_gui::Platform) implementation for the [`Desktop`](Desktop) platform.
+mod desktop {
 
-#[async_trait]
-impl Platform for Desktop {
-    fn is_nfd_available() -> bool {
-        true
-    }
+    use std::path::PathBuf;
 
-    async fn nfd_choose_folder() -> Result<PathBuf, pwduck_gui::error::NfdError> {
-        let folder = AsyncFileDialog::new()
-            .set_directory(
-                dirs::document_dir()
-                    .unwrap_or_else(|| dirs::home_dir().unwrap_or_else(|| "".into())),
-            )
-            .pick_folder()
-            .await
+    use async_trait::async_trait;
+    use enigo::KeyboardControllable;
+    use rfd::AsyncFileDialog;
+
+    use pwduck_gui::{
+        error::{NfdError, PWDuckGuiError},
+        Platform, Sequence,
+    };
+
+    /// An empty placeholder struct to implement the [`Platform`](Platform) trait for.
+    #[derive(Default)]
+    pub struct Desktop;
+
+    #[async_trait]
+    impl Platform for Desktop {
+        fn is_nfd_available() -> bool {
+            true
+        }
+
+        async fn nfd_choose_folder() -> Result<PathBuf, pwduck_gui::error::NfdError> {
+            let folder = AsyncFileDialog::new()
+                .set_directory(
+                    dirs::document_dir()
+                        .unwrap_or_else(|| dirs::home_dir().unwrap_or_else(|| "".into())),
+                )
+                .pick_folder()
+                .await
+                .ok_or(NfdError::Null)?;
+
+            Ok(folder.path().into())
+        }
+
+        async fn nfd_choose_key_file(
+            file_name: Option<String>,
+        ) -> Result<PathBuf, pwduck_gui::error::NfdError> {
+            let key_file = AsyncFileDialog::new()
+                .set_directory(
+                    dirs::document_dir()
+                        .unwrap_or_else(|| dirs::home_dir().unwrap_or_else(|| "".into())),
+                )
+                .add_filter("PWDuck key file", &["pwdk"]);
+
+            let key_file = if let Some(file_name) = file_name {
+                key_file.set_file_name(&file_name).save_file().await
+            } else {
+                key_file.pick_file().await
+            }
             .ok_or(NfdError::Null)?;
 
-        Ok(folder.path().into())
-    }
-
-    async fn nfd_choose_key_file(
-        file_name: Option<String>,
-    ) -> Result<PathBuf, pwduck_gui::error::NfdError> {
-        let key_file = AsyncFileDialog::new()
-            .set_directory(
-                dirs::document_dir()
-                    .unwrap_or_else(|| dirs::home_dir().unwrap_or_else(|| "".into())),
-            )
-            .add_filter("PWDuck key file", &["pwdk"]);
-
-        let key_file = if let Some(file_name) = file_name {
-            key_file.set_file_name(&file_name).save_file().await
-        } else {
-            key_file.pick_file().await
-        }
-        .ok_or(NfdError::Null)?;
-
-        Ok(key_file.path().into())
-    }
-
-    fn is_open_in_browser_available() -> bool {
-        true
-    }
-
-    async fn open_in_browser(url: String) -> Result<(), pwduck_gui::error::PWDuckGuiError> {
-        opener::open_browser(url)
-            .map_err(|err| pwduck_gui::error::PWDuckGuiError::String(format!("{}", err)))?;
-        Ok(())
-    }
-
-    fn is_auto_type_available() -> bool {
-        true
-    }
-
-    async fn auto_type(sequence: Sequence) -> Result<(), pwduck_gui::error::PWDuckGuiError> {
-        async_std::task::sleep(std::time::Duration::from_millis(1000)).await;
-
-        let mut enigo = enigo::Enigo::new();
-        #[cfg(target_os = "linux")]
-        {
-            enigo.set_delay(25);
-
-            // Check if xdotools is available
-            // TODO: maybe replace this by `get_program` in the future.
-            // <https://doc.rust-lang.org/std/process/struct.Command.html#method.get_program>
-            drop(
-                std::process::Command::new("xdotool")
-                    .output()
-                    .map_err(|_err| PWDuckGuiError::XDOToolsMissing)?,
-            );
+            Ok(key_file.path().into())
         }
 
-        for part in sequence.iter() {
-            match part {
-                pwduck_gui::Part::Literal(literal) => enigo.key_sequence(literal),
-                pwduck_gui::Part::Field(field) => enigo.key_sequence(field),
-                pwduck_gui::Part::Key(key) => match key {
-                    pwduck_gui::Key::Tab => enigo.key_click(enigo::Key::Tab),
-                    pwduck_gui::Key::Return => enigo.key_click(enigo::Key::Return),
-                },
+        fn is_open_in_browser_available() -> bool {
+            true
+        }
+
+        async fn open_in_browser(url: String) -> Result<(), pwduck_gui::error::PWDuckGuiError> {
+            opener::open_browser(url)
+                .map_err(|err| pwduck_gui::error::PWDuckGuiError::String(format!("{}", err)))?;
+            Ok(())
+        }
+
+        fn is_auto_type_available() -> bool {
+            true
+        }
+
+        async fn auto_type(sequence: Sequence) -> Result<(), pwduck_gui::error::PWDuckGuiError> {
+            async_std::task::sleep(std::time::Duration::from_millis(1000)).await;
+
+            let mut enigo = enigo::Enigo::new();
+            #[cfg(target_os = "linux")]
+            {
+                enigo.set_delay(25);
+
+                // Check if xdotools is available
+                // TODO: maybe replace this by `get_program` in the future.
+                // <https://doc.rust-lang.org/std/process/struct.Command.html#method.get_program>
+                drop(
+                    std::process::Command::new("xdotool")
+                        .output()
+                        .map_err(|_err| PWDuckGuiError::XDOToolsMissing)?,
+                );
             }
-        }
 
-        Ok(())
+            for part in sequence.iter() {
+                match part {
+                    pwduck_gui::Part::Literal(literal) => enigo.key_sequence(literal),
+                    pwduck_gui::Part::Field(field) => enigo.key_sequence(field),
+                    pwduck_gui::Part::Key(key) => match key {
+                        pwduck_gui::Key::Tab => enigo.key_click(enigo::Key::Tab),
+                        pwduck_gui::Key::Return => enigo.key_click(enigo::Key::Return),
+                    },
+                }
+            }
+
+            Ok(())
+        }
     }
 }
